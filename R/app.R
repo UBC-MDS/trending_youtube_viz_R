83f9849bfd7f8d13a7ed9c1bb053439205acd063
Lauren Zung <lzung989@gmail.com>
2023-03-02 13:01:45 -0800

    add themes

--- R/app.R
+++ R/app.R
@@ -8,97 +8,168 @@ library(thematic)
 library(scales)
 library(rlang)
 library(stringr)
+library(showtext)
+library(thematic)
+library(sysfonts)
 
-ui <- navbarPage("YouTube Trend Visualizer",
-  theme = bs_theme(bootswatch = "lux"),
-  sidebarLayout(
-    sidebarPanel(
-      width = 3,
-      dateRangeInput(
-        inputId = "daterange",
-        label = "Trending Date Range:",
-        start = min(data$trending_date),
-        end = max(data$trending_date),
-        format = "yyyy-mm-dd"
-      )
-    ),
-    mainPanel(
-      fluidRow(
-        column(3,
-          align = "center", offset = 0.5,
-          style = "padding:40px;", "Category Boxplots"
-        ),
-        column(
-          3,
-          selectInput(
-            inputId = "boxplotdist",
-            label = "Distribution Metric:",
-            choices = c(
-              "Comments (in thousands)" = "comment_count",
-              "Dislikes (in thousands)" = "dislikes",
-              "Likes (in thousands)" = "likes",
-              "Views (in millions)" = "view_count"
-            ),
-            selected = "Comments (in thousands)"
+data <- read.csv('../data/processed/CA_youtube_trending_data_processed.csv')
+
+boxplot_options <- c(
+  "Comments" = "comment_count",
+  "Dislikes" = "dislikes",
+  "Likes" = "likes",
+  "Views" = "view_count"
+)
+
+boxplot_colours <- setNames(
+  c("#35618f", "#2dadb8", "#2a6a45", "#0df38f", "#93c680",
+    "#21a708", "#bce333", "#7e2b19", "#de592e", "#fcd107",
+    "#b08965", "#d4d4d4", "#5c51b1", "#cc99d9", "#a53bb7"),
+  unique(data$categoryId)
+)
+
+barplot_colours <- setNames(
+  unique(data$categoryId),
+  c("#35618f", "#2dadb8", "#2a6a45", "#0df38f", "#93c680",
+    "#21a708", "#bce333", "#7e2b19", "#de592e", "#fcd107",
+    "#b08965", "#d4d4d4", "#5c51b1", "#cc99d9", "#a53bb7")
+)
+
+main_theme <- bslib::bs_theme(
+  bootswatch = "journal",
+  base_font = bslib::font_google("Assistant"))
+
+# Add fonts from Google
+sysfonts::font_add_google("Assistant")
+
+# Automatically use showtext to render text
+showtext::showtext_auto()
+
+# Let thematic know to update the plot fonts too
+thematic::thematic_shiny(font = "auto")
+
+ui <- fluidPage(theme = main_theme,
+  navbarPage(
+    theme = main_theme,
+    
+    # Import Font Awesome icons
+    tags$style("@import url(https://use.fontawesome.com/releases/v5.7.2/css/all.css);"),
+    
+    # tags$head(
+    #   tags$style(HTML("
+    #     .navbar-static-top {
+    #       background-color: #E96161;
+    #     }"))
+    # ),
+    
+    title = span(icon("youtube"), "YouTube Trend Visualizer"),
+    
+    tabPabel(
+      "Options",
+      sidebarLayout(
+        sidebarPanel(
+          width = 2,
+          dateRangeInput(
+            inputId = "daterange",
+            label = "Trending Date Range:",
+            start = min(data$trending_date),
+            end = max(data$trending_date),
+            format = "yyyy-mm-dd"
+          ),
+          radioButtons(
+            inputId = "toggle_theme",
+            label = "Mode",
+            choices = c("Light" = "journal", "Dark" = "darkly")),
           )
-        ),
-        column(3,
-          align = "center", offset = 0.5,
-          style = "padding:40px;", "Channel Barplot"
-        ),
-        column(
-          3,
-          selectInput(
-            inputId = "barplotcat",
-            label = "Category:",
-            choices = unique(data$categoryId),
-            selected = "Film & Animation"
-          )
-        )
       ),
-      fluidRow(
-        column(
-          6,
-          plotlyOutput(outputId = "boxplot")
-        ),
-        column(
-          6,
-          plotlyOutput(outputId = "barplot")
+      mainPanel(
+        width = 10,
+        # fluidRow(
+        #   column(3,
+        #     align = "center", offset = 0.5,
+        #     style = "padding:40px;", "Category Boxplots"
+        #   ),
+        #   column(
+        #     3,
+        #     selectInput(
+        #       inputId = "boxplotdist",
+        #       label = "Distribution Metric:",
+        #       choices = c(
+        #         "Comments (in thousands)" = "comment_count",
+        #         "Dislikes (in thousands)" = "dislikes",
+        #         "Likes (in thousands)" = "likes",
+        #         "Views (in millions)" = "view_count"
+        #       ),
+        #       selected = "Comments (in thousands)"
+        #     )
+        #   ),
+        #   column(3,
+        #     align = "center", offset = 0.5,
+        #     style = "padding:40px;", "Channel Barplot"
+        #   ),
+        #   column(
+        #     3,
+        #     selectInput(
+        #       inputId = "barplotcat",
+        #       label = "Category:",
+        #       choices = unique(data$categoryId),
+        #       selected = "Film & Animation"
+        #     )
+        #   )
+        # ),
+        # fluidRow(
+        #   column(
+        #     6,
+        #     plotlyOutput(outputId = "boxplot")
+        #   ),
+        #   column(
+        #     6,
+        #     plotlyOutput(outputId = "barplot")
+        #   )
+        # ),
+        layout_column_wrap(
+          width = 1/2,
+          card(
+            full_screen = TRUE,
+            card_header("Category Boxplots"),
+            card_body_fill(
+              selectInput(
+                inputId = "boxplotdist",
+                label = "Distribution Metric:",
+                choices = boxplot_options,
+                selected = "Comments"
+              ),
+              plotlyOutput(outputId = "boxplot")
+            )
+          ),
+          card(
+            full_screen = TRUE,
+            card_header("Channel Barplot"),
+            card_body_fill(
+              selectInput(
+                inputId = "barplotcat",
+                label = "Category:",
+                choices = unique(data$categoryId),
+                selected = "Music"
+              ),
+              plotlyOutput(outputId = "barplot")
+            )
+          )
         )
       )
     )
   )
-)
 
 server <- function(input, output, session) {
-  # Select List Input Control
-  output$data <- renderTable(
-    {
-      dataset[, c("dataset_column_name1", input$variable), drop = FALSE]
-    },
-    rownames = TRUE
-  )
-
-  scaleInput <- reactive({
-    if (input$boxplotdist == "view_count") {
-      return(1e-6)
-    } else {
-      return(1e-4)
-    }
+  # Dark Mode
+  observe({
+    session$setCurrentTheme(
+      bs_theme_update(main_theme, bootswatch = input$toggle_theme)
+    )
   })
-
-  suffixInput <- reactive({
-    if (input$boxplotdist == "view_count") {
-      return("M")
-    } else {
-      return("K")
-    }
-  })
-
+  
   # Category Boxplot
   output$boxplot <- plotly::renderPlotly({
-    thematic::thematic_shiny()
-
     plotly::ggplotly(
       data |>
         dplyr::filter(trending_date > input$daterange[1] & trending_date < input$daterange[2]) |>
@@ -112,7 +183,12 @@ server <- function(input, output, session) {
             fill = categoryId
           )
         ) +
-        ggplot2::scale_y_continuous(labels = label_number(suffix = suffixInput(), scale = scaleInput())) +
+        ggplot2::labs(
+          x = names(boxplot_options[which(boxplot_options == input$boxplotdist)]),
+          y = 'Category',
+        ) +
+        ggplot2::scale_y_continuous(labels = scales::label_number(scale_cut = cut_short_scale()), breaks = scales::breaks_pretty(n = 5)) +
+        ggplot2::scale_fill_manual(values = boxplot_colours) + 
         ggplot2::guides(fill = FALSE) +
         ggplot2::coord_flip()
     )
@@ -120,8 +196,6 @@ server <- function(input, output, session) {
 
   # Channel Barplot
   output$barplot <- plotly::renderPlotly({
-    thematic::thematic_shiny()
-
     plotly::ggplotly(
       data |>
         dplyr::filter(trending_date > input$daterange[1] & trending_date < input$daterange[2]) |>
@@ -137,7 +211,11 @@ server <- function(input, output, session) {
             y = channelTitle
           )
         ) +
-        ggplot2::geom_bar(stat = "identity") +
+        ggplot2::geom_bar(stat = "identity", fill = names(barplot_colours[which(barplot_colours == input$barplotcat)])) +
+        ggplot2::labs(
+          x = 'Count of Videos',
+          y = 'Channel Title'
+        ) +
         ggplot2::scale_y_discrete(labels = function(x) {
           stringr::str_wrap(x, width = 15)
         })
